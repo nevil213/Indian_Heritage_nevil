136e46e1018b4149a85c3c2c60852b1cd05e39c4
Nevil <nevilvataliya84@gmail.com>
2024-07-15 12:27:51 +0530

    added tested files to main file

--- html/script.js
+++ html/script.js
@@ -1,19 +1,16 @@
 window.onload = function() {
   setTimeout(function() {
     document.querySelector('.one').classList.add('show1');
-  }, 1900);
+  }, 2145);
   setTimeout(function() {
     document.querySelector('.two').classList.add('show2');
-  }, 1900);
+  }, 2145);
 };
 
-
-  $('.BornHS__Input').change(function(){
-    if($(this).is(":checked")) {
-        $('.BornHS__Wrapper').addClass("BornHS__Blur");
-        $(this).closest('.BornHS__Wrapper').removeClass("BornHS__Blur");
-    } else {
-        $('.BornHS__Wrapper').removeClass("BornHS__Blur");
-    }
+$('.BornHS__Input').change(function(){
+  if($(this).is(":checked")) {
+      $('.BornHS__Wrapper').addClass("BornHS__Blur");
+  } else {
+      $('.BornHS__Wrapper').removeClass("BornHS__Blur");
+  }
 });
-
