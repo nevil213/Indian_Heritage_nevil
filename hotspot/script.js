4ee486ee083a9a7768dc9c39d6b9c3c4d86eeaef
Nevil <nevilvataliya84@gmail.com>
2024-07-23 23:19:06 +0530

    folder name changed

--- hotspot/script.js
+++
@@ -1,7 +0,0 @@
-$('.BornHS__Input').change(function(){
-    if($(this).is(":checked")) {
-        $('.BornHS__Wrapper').addClass("BornHS__Blur");
-    } else {
-        $('.BornHS__Wrapper').removeClass("BornHS__Blur");
-    }
-});
--- html/scroll/script.js
+++
@@ -1,8 +0,0 @@
-function scrollToSection(sectionId) {
-  var section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: 'smooth' });
-}
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
