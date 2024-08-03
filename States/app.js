22cd6257a1d676cb9822bc40c2fc673c70c0da88
Nevil <nevilvataliya84@gmail.com>
2024-08-03 20:50:12 +0530

    cursor effect added

--- States/app.js
+++ States/app.js
@@ -87,3 +87,45 @@ window.addEventListener('scroll', function () {
         arrows.style.animation = 'thumnarrows_show 0.3s linear forwards';
     }
 });
+
+// Select the .mycircle element
+let cursor = document.querySelector('.mycircle');
+
+// Function to add .hover class
+function addHoverClass() {
+    cursor.classList.add('hover');
+}
+
+// Function to remove .hover class
+function removeHoverClass() {
+    cursor.classList.remove('hover');
+}
+
+// Select all elements with .type, .heritage, and .state classes
+let hoverElements = document.querySelectorAll('.type, .list .heritage, .state');
+
+// Add event listeners to each element
+hoverElements.forEach(element => {
+    element.addEventListener('mouseenter', addHoverClass);
+    element.addEventListener('mouseleave', removeHoverClass);
+});
+
+
+// Function to add .hover class
+function addDifferenceClass() {
+    cursor.classList.add('cursor_difference');
+}
+
+// Function to remove .hover class
+function removeDifferencerClass() {
+    cursor.classList.remove('cursor_difference');
+}
+
+// Select all elements with .type, .heritage, and .state classes
+let hoverImage = document.querySelectorAll('nav, .des, .thumbnail .heritage, button, footer');
+
+// Add event listeners to each element
+hoverImage.forEach(element => {
+    element.addEventListener('mouseenter', addDifferenceClass);
+    element.addEventListener('mouseleave', removeDifferencerClass);
+});
