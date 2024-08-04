a281ccd2afa992caec5411704012fa86d72be9a5
Nevil <nevilvataliya84@gmail.com>
2024-08-04 21:05:43 +0530

    added curser effect and footer to other states and sections and added subscription email

--- src/common.js
+++ src/common.js
@@ -163,3 +163,20 @@ window.addEventListener('scroll', function () {
         arrows.style.animation = 'thumnarrows_show 0.3s linear forwards';
     }
 });
+
+document.getElementById('subscribeForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    var name = document.querySelector('input[name="name"]').value;
+    var email = document.querySelector('input[name="email"]').value;
+
+    fetch('https://script.google.com/macros/s/AKfycby5vTUOI8xfPR--x_WXL7RCYRswEsUuol1ImstKCt_m0K4uafpU_6aStjTMouPx-CiO/exec', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({ 'name': name, 'email': email })
+    }).then(response => {
+        alert('Thank you for subscribing!');
+    }).catch(error => console.error('Error!', error.message));
+}
